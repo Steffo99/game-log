56c47814cde571e6bdcced8b817a19bea90523b4
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-09-03 19:46:26 +0200

    ,,

--- api.py
+++ api.py
@@ -110,7 +110,7 @@ def api_v1_user_search():
             })
         return f.jsonify({
             "result": "success",
-            "description": "Retrieved user successfully."
+            "description": "Retrieved user successfully.",
             "user": db_user.json()
         })
     return f.jsonify({
